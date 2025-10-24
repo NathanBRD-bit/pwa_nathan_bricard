295c9c096bf173a6717e12f87543744ceb23ac8d
Nathan Bricard <bricardnathan@gmail.com>
2025-10-24 14:04:04 +0200

    test-cd

--- src/components/roomList.tsx
+++ src/components/roomList.tsx
@@ -70,7 +70,7 @@ export default function RoomList() {
         <div className="p-6">
             {!isConnected ? (
                 <div className="space-y-4">
-                    <h2 className="text-lg font-medium">Connexionezefrfrferferfef</h2>
+                    <h2 className="text-lg font-medium">Connexion</h2>
                     <input
                         type="text"
                         placeholder="Votre pseudo"
